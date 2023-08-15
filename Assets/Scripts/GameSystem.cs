56e600e726cd55fc636d24c480f25104f8dfbd2e
AkiraToya <akira_toya2006@yahoo.com>
2023-08-15 13:39:09 +0700

    18.4. Gameover, Lives, Retry feaatures

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -1,3 +1,4 @@
+using System.Net.Sockets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,24 +14,55 @@ public class GameSystem : MonoBehaviour
     private SpawnArea spawnArea;
 
     [SerializeField]
+    private LivesControl livesControl;
+
+    [SerializeField]
+    private GameObject gameOverScreen;
+
+    [SerializeField]
     private TMP_Text scoreText;
+
+    [SerializeField]
+    private TMP_Text finalScoreText;
 
     private GameObject currentBall;
     private SceneSystem sceneSystem = new SceneSystem();
 
     private int score = 0;
+    private bool isScored = false;
+    private bool isGameOver = false;
 
     private void Awake() {
         sceneSystem.init();
+        livesControl.onGameOverEvent += onGameOver;
     }
 
     public void scored(){
+        isScored = true;
         score += 1;
+        setScore();
+    }
+
+    private void setScore(){
         scoreText.SetText(score.ToString());
     }
 
+    private void setFinalScore(){
+        finalScoreText.SetText("Score: " + score.ToString());
+    }
+
     public void startGame(){
+        score = 0;
+        setScore();
+        isGameOver = false;
         spawnBall();        
+    }
+
+    private void onGameOver(){
+        print("Game Over");
+        isGameOver = true;
+        setFinalScore();
+        gameOverScreen.SetActive(true);
     }
 
     public Scene getScenePrediction(){
@@ -42,8 +74,16 @@ public class GameSystem : MonoBehaviour
     }
 
     private void respawnBall(){
+        if(!isScored)
+            livesControl.loseLive();
+        
+        isScored = false;
         destroyBall();
         spawnBall();
+    }
+
+    private void checkGameOver(){
+        if(isGameOver) destroyBall();
     }
 
     private void spawnBall(){
@@ -52,6 +92,7 @@ public class GameSystem : MonoBehaviour
 
         ballCommand.scoredEvent += scored;
         ballCommand.onGroundEvent += respawnBall;
+        ballCommand.onGroundEvent += checkGameOver;
 
         spawnArea.spawnBall(currentBall.transform);
     }
